3640e753041e2bc76c24622b7e46afaac8c6ac6d
The architect <Sebastianmuchui79@gmail.com>
2021-07-24 16:30:18 +0300

    If i was a program....

---
+++ seb.ino
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace seb;
+int main(){
+	string name = "sebastian";
+	
+	
+	char email = "7iastian@gmail.com";
+	
+	
+	string favLanguages = {'Javascript','python','php','c++'};
+	
+	string location = "254";
+	
+	string exact_location = "254.012";
+	
+	string Favmeme = "Assume a spherical cow of uniform density while ignoring the effects of gravity in a vacuum";
+	
+	bool iamcool = false;
+	
+	bool iamsocial = false;
+	
+	bool im_a_nerd = true;
+	
+	string favProject = 'thescriptslayer/rainy-mail';
+	
+	string nickname = "The architect";
+	
+		string motivation = "When all else fails assume a spherical cow in a vacuum";
+	
+}

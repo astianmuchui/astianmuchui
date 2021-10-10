61d1a50a8b541f05d3b9cdbe746cca18dc646c89
Sebastian <Sebastianmuchui79@gmail.com>
2021-10-10 08:46:24 +0300

    Update seb.ino

--- seb.ino
+++ seb.ino
@@ -2,29 +2,16 @@
 using namespace seb;
 int main(){
 	string name = "sebastian";
-	
-	
 	char email = "7iastian@gmail.com";
-	
-	
 	string favLanguages = {'Javascript','python','php','c++'};
-	
 	string location = "254";
-	
 	string exact_location = "254.012";
-	
 	string Favmeme = "Assume a spherical cow of uniform density while ignoring the effects of gravity in a vacuum";
-	
-	bool iamcool = false;
-	
+	bool iamcool = true;
 	bool iamsocial = false;
-	
 	bool im_a_nerd = true;
-	
-	string favProject = 'thescriptslayer/rainy-mail';
-	
-	string nickname = "The architect";
-	
-		string motivation = "When all else fails assume a spherical cow in a vacuum";
+	string favProject = 'Talkit';
+	string nickname = NULL;
+        string motivation = "When all else fails assume a spherical cow in a vacuum";
 	
 }

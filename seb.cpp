526d675947097a467d835c16573f3be3aa9f122b
Sebastian M <Sebastianmuchui79@gmail.com>
2022-06-14 08:14:12 +0300

    Update and rename seb.ino to seb.cpp

--- seb.cpp
+++ seb.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
 	string name = "sebastian";
 	char email = "7iastian@gmail.com";
-	string favLanguages = {'Javascript','python','php','c++'};
+	string favLanguages = {'Javascript','python','php','c++','c','ruby'};
 	string location = "254";
 	string exact_location = "254.012";
 	string Favmeme = "Assume a spherical cow of uniform density while ignoring the effects of gravity in a vacuum";
